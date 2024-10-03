d41741bc31087c0d0c1c7999156a327572c71c50
likaon1606 <ariel.fuentesgarcia@gmail.com>
2024-10-03 12:42:36 -0600

    about__gray-section responsive finish

--- page-templates/02. AcercaDe.php
+++ page-templates/02. AcercaDe.php
@@ -4,36 +4,43 @@
 */
 ?>
 
-<section class="about__me">
-  <?php get_header(); ?>
+<?php get_header(); ?>
 
-  <div class="about__content">
-    <h1 class="about__title">ARHEA<br>PROPERTY<br>DEVELOPMENT</h1>
-    <p class="about__subtitle">Somos una <strong> desarrolladora de proyectos inmobiliarios</strong><br> comprometidos a crear ambientes cálidos, acogedores y que <br> fomenten el bienestar de sus habitantes.</p>
-    
-    <div class="about__submenu-link">
-      <button class="about__projects-btn">CONOCER PROYECTOS</button>
-      <button class="about__info-btn">Más información</button>
+<section class="about__me">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="about__content">
+          <h1 class="about__title">ARHEA<br>PROPERTY<br>DEVELOPMENT</h1>
+          <p class="about__subtitle">Somos una <strong> desarrolladora  de proyectos inmobiliarios</strong><br> comprometidos a crear ambientes cálidos, acogedores y que <br> fomenten el bienestar de sus habitantes.</p>
+          <div class="about__submenu-link">
+            <button class="about__projects-btn">CONOCER PROYECTOS</button>
+            <button class="about__info-btn">Más información</button>
+          </div>
+          <?php echo siteImage("banner__about", "about__image", "Imagen de about Principal"); ?>
+        </div>
+      </div>
     </div>
   </div>
-  
-  <?php echo siteImage("banner__about", "about__image", "Imagen de about Principal"); ?>
 </section>
 
 <section class="about__gray-section">
-<div class="container">
+  <div class="container">
     <div class="row">
       <div class="col-12">
         <div class="about__content-gray">
           <div class="about__gray-title">
-            <h1>En <span>Arhea Property <br>Development</span> <br>entendemos <br>que un hogar <br>es más que <br>espacio físico</h1>
+            <h1>
+              En <span>Arhea Property Development</span> entendemos que un hogar es más que espacio físico
+            </h1>
           </div>
           <div class="about__gray-description">
             <p>Un hogar es un santuario donde se crean  recuerdos, se fortalecen lazos familiares, se hacen realidad sueños y se transforman vidas. 
             </p>
             <p>Creemos que todos merecen habitar en un hogar que los inspire a crecer y prosperar, ya sea persiguiendo una pasión, compartiendo momentos con seres queridos o simplemente encontrando paz y tranquilidad en su propio espacio.
             </p>
-            <p>NUESTRAS RESIDENCIAS ESTÁN DISEÑADAS PARA SACAR LO MEJOR DE LAS PERSONAS.
+            <p>
+              NUESTRAS RESIDENCIAS ESTÁN DISEÑADAS PARA SACAR LO MEJOR DE LAS PERSONAS.
             </p>
           </div>
         </div>
@@ -41,7 +48,7 @@
     </div>
   </div>
 </section>
-
+<!-- 
 <section class="about__img-simple">
   <div class="container-fluid">
     <?php echo siteImage("img-simple__about", "about__image-simple", "Imagen de about simple sin textos"); ?>
@@ -120,7 +127,7 @@
     </div>
   </div>
   </div>
-</section>
+</section> -->
 
 
 
