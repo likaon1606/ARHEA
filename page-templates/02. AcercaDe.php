2de630931b3a96414c64845727f768d0e042ea17
likaon1606 <ariel.fuentesgarcia@gmail.com>
2024-10-04 12:59:48 -0600

    acerca de responsive complete

--- page-templates/02. AcercaDe.php
+++ page-templates/02. AcercaDe.php
@@ -35,9 +35,11 @@
             </h1>
           </div>
           <div class="about__gray-description">
-            <p>Un hogar es un santuario donde se crean  recuerdos, se fortalecen lazos familiares, se hacen realidad sueños y se transforman vidas. 
+            <p>
+              Un hogar es un santuario donde se crean  recuerdos, se fortalecen lazos familiares, se hacen realidad sueños y se transforman vidas.
             </p>
-            <p>Creemos que todos merecen habitar en un hogar que los inspire a crecer y prosperar, ya sea persiguiendo una pasión, compartiendo momentos con seres queridos o simplemente encontrando paz y tranquilidad en su propio espacio.
+            <p>
+              Creemos que todos merecen habitar en un hogar que los inspire a crecer y prosperar, ya sea persiguiendo una pasión, compartiendo momentos con seres queridos o simplemente encontrando paz y tranquilidad en su propio espacio.
             </p>
             <p>
               NUESTRAS RESIDENCIAS ESTÁN DISEÑADAS PARA SACAR LO MEJOR DE LAS PERSONAS.
@@ -48,11 +50,9 @@
     </div>
   </div>
 </section>
-<!-- 
-<section class="about__img-simple">
-  <div class="container-fluid">
-    <?php echo siteImage("img-simple__about", "about__image-simple", "Imagen de about simple sin textos"); ?>
-  </div>
+
+<section class="about__container-img">
+        <?php echo siteImage("img-simple__about", "about__image-simple", "Imagen de about simple sin textos"); ?>
 </section>
 
 <section class="about__values">
@@ -61,7 +61,7 @@
         <div class="col-12">
           <div class="about__values-content">
             <h1>¿QUÉ NOS HACE DIFERENTES?</h1>
-            <p class="about__subtitle">En <strong>Arhea Property Development</strong> trabajamos todos los días para que encuentres el <br>hogar de tus sueños.
+            <p class="about__values-subtitle">En <strong>Arhea Property Development</strong> trabajamos todos los días para que encuentres el <br>hogar de tus sueños.
             <div class="about__mision-vision">
               <div class="about__mision">
                 <h3>MISIÓN</h3>
@@ -85,41 +85,42 @@
   <div class="about__image-half">
 
     <div class="about__black">
-      <div class="__black-description">
+      <div class="__black-description-left">
         <h1>CORE VALUES</h1>
-        <p>Desafiamos constantemente los límites del diseño para crear <br>hogares funcionales y estéticamente inspiradores.
+        <p>Desafiamos constantemente los límites del diseño para crear hogares funcionales y estéticamente inspiradores.
         </p>
         <button class="__black-description-btn">VER PROYECTOS</button>
       </div>
+
       <div class="__black-description">
         <h2>ATENCIÓN AL DETALLE</h2>
         <h2 class="__black-separator">DISEÑO</h2>
         <h2>CALIDAD</h2>
       </div>
     </div>
-
+  
       <?php echo siteImage("image__half", "about__image-in-half", "Imagen dividida a la mitad entre dos contenedores"); ?>
 
     <div class="about__blue">
       <h1>CONOCE A NUESTRO <br>EQUIPO DIRECTIVO</h1>
-      <p class="__about__blue-subtitle">Poseemos más de dos <strong>décadas de conocimiento en el ámbito de bienes raíces,</strong> respaldados por un grupo<br>de profesionales enfocados en los proyectos y sus residentes.
+      <p class="__about__blue-subtitle">Poseemos más de dos <strong>décadas de conocimiento en el ámbito de bienes raíces,</strong> respaldados por un grupo de profesionales enfocados en los proyectos y sus residentes.
       </p>
       
       <div class="__blue-content">
         <div class="__blue-content-ganem">
           <?php echo siteImage("ganem", "__ganem", "Imagen de HECTOR AREHIGA GANEM"); ?>
           <div class="__ganem-description">
-            <h4>HÉCTOR<br>AÉCHIGA <br>GANEM</h4>
-            <p>Arhea Property<br>Development</p>
-            <p>Fundador, Presidente y Consejero<br>Delegado</p>
+            <h4>HÉCTOR AÉCHIGA GANEM</h4>
+            <p>Arhea Property Development</p>
+            <p>Fundador, Presidente y Consejero Delegado</p>
           </div>
         </div>
 
         <div class="__blue-content-saiz">
           <?php echo siteImage("saiz", "__saiz", "Imagen de JOSÉ LUIS SAÍZ FERNÁNDEZ"); ?>
           <div class="__saiz-description">
-            <h4>JOSÉ LUIS SAÍZ<br>FERNÁNDEZ</h4>
-            <p>Arhea Property<br>Development</p>
+            <h4>JOSÉ LUIS SAÍZ FERNÁNDEZ</h4>
+            <p>Arhea Property Development</p>
             <p>VP - Unidad de Negocio Multifamiliar</p>
           </div>
         </div>
@@ -127,7 +128,7 @@
     </div>
   </div>
   </div>
-</section> -->
+</section>
 
 
 
